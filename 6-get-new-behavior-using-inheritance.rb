ac83ce6b9963d9d43d539c8d331e9f0130663183
구건모 <rnrjsah789@tmon.co.kr>
2018-10-14 18:56:00 +0900

    hook을 사용하여 추상화, super에 대한 의존성 감소

--- 6-get-new-behavior-using-inheritance.rb
+++ 6-get-new-behavior-using-inheritance.rb
@@ -5,6 +5,7 @@ class Bicycle
     @size = args[:size]
     @chain = args[:chain] || default_chain
     @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
   end  
 
   def default_chain
@@ -19,24 +20,25 @@ class Bicycle
     {
       chain: chain,
       tire_size: tire_size
-    }
+  }.merge(local_spares)
   end
 end
 
 class RoadBike < Bicycle
   attr_reader :tape_color
 
-  def initialize(args)
+  def post_initialize(args)
     @tape_color = args[:tape_color]
-    super(args)
   end
 
   def default_tire_size
     '23'
   end
   
-  def spares
-    super.merge(tape_color: tape_color)
+  def local_spares
+    {
+      tape_color: tape_color
+    }
   end
 
 end
@@ -44,18 +46,20 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shork
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shork = args[:rear_shork]
-    super(args)
   end
 
   def default_tire_size
     '2.1'
   end
 
-  def spares
-    super.merge(rear_shork: rear_shork, front_shock: front_shock)
+  def local_spares
+    {
+      rear_shork: rear_shork, 
+      front_shock: front_shock
+    }
   end
 
 end
@@ -63,7 +67,7 @@ end
 class RecumbentBike < Bicycle
   attr_reader :flag
 
-  def initialize(args)
+  def post_initialize(args)
     @flag = args[:flag]
   end
 
@@ -71,8 +75,10 @@ class RecumbentBike < Bicycle
     '28'
   end
 
-  def spares
-    super.merge(flag: flag)
+  def local_spares
+    {
+      flag: flag
+    }
   end
 end
 

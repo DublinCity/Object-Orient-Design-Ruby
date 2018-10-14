65c43e600be3bce73db23b2cea9c15826f5ab8d0
구건모 <rnrjsah789@tmon.co.kr>
2018-10-14 17:47:08 +0900

    Revert "안티패턴, 객체의 클래스를 확인하여 상속을 구현"
    This reverts commit fea11148f8005195b62fe87854c78405cc84a1cf.

--- 6-get-new-behavior-using-inheritance.rb
+++ 6-get-new-behavior-using-inheritance.rb
@@ -1,36 +1,23 @@
 class Bicycle
-    attr_reader :size, :tape_color, :style,
-    :front_shock, :rear_shork
+    attr_reader :size, :tape_color
 
     def initialize(args)
       @size = args[:size]
       @tape_color = args[:tape_color]
-      @style = args[:style]
-      @front_shock = args[:front_shock]
-      @rear_shork = args[:rear_shork]
     end
 
     def spares
-      if style == :road
-        {
-          chain: '10-speed',
-          tire_size: '23',
-          tape_color: tape_color
-        }
-      else
-        {
-          chain: '10-speed',
-          tire_size: '2.1',
-          rear_shork: rear_shork,
-        }
-      end
+      {
+        chain: '10-speed',
+        tire_size: '23',
+        tape_color: tape_color
+      }
     end
 end
 
 bike = Bicycle.new(
-  size: 'S',
-  style: :mountain,
-  front_shock: 'Manitou',
-  rear_shork: 'Fox')
+  size: 'M',
+  tape_color: 'red')
 
+puts(bike.size)
 puts(bike.spares)

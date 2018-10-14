d8d605b399fe643c0638d147613c01de114c28b6
Unknown <rnrjsah789@tmon.co.kr>
2018-10-14 18:14:21 +0900

    탬플릿 메서드 패턴
    기본 구조를 상위클래스가 정의하고 상위클래스에서 메세지를 전송하여 하위 클래스의 값을 가진다

--- 6-get-new-behavior-using-inheritance.rb
+++ 6-get-new-behavior-using-inheritance.rb
@@ -1,22 +1,34 @@
 class Bicycle
-    
-end
-
-class RoadBike < Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :size, :chain, :tire_size
 
   def initialize(args)
     @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+  end  
+
+  def default_chain
+    '10-speed'
+  end
+
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def initialize(args)
     @tape_color = args[:tape_color]
+    super(args)
+  end
+
+  def default_tire_size
+    '23'
   end
 
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    super.merge(tape_color: tape_color)
   end
+
 end
 
 class MountainBike < Bicycle
@@ -28,14 +40,27 @@ class MountainBike < Bicycle
       super(args)
     end
 
-    def spares
-      super.merge(rear_shork: rear_shork)
+    def default_tire_size
+      '2.1'
     end
+
+    def spares
+      super.merge(rear_shork: rear_shork, front_shock: front_shock)
+    end
+
 end
+
+roadBike = RoadBike.new(
+  size: 'M',
+  tape_color: 'red')
 
 mountainBike = MountainBike.new(
   size: 'S',
   front_shock: 'Manitou',
   rear_shork: 'Fox')
 
-puts(mountainBike.spares)
+puts(roadBike.tire_size)
+puts(roadBike.chain)
+
+puts(mountainBike.tire_size)
+puts(mountainBike.chain)

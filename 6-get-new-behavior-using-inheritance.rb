4a1a638a44af95e45b162fe0b1ddc8131e945157
구건모 <rnrjsah789@tmon.co.kr>
2018-10-14 17:55:57 +0900

    추상화 코드 모두 구체클래스로 내리기

--- 6-get-new-behavior-using-inheritance.rb
+++ 6-get-new-behavior-using-inheritance.rb
@@ -1,18 +1,22 @@
 class Bicycle
-    attr_reader :size, :tape_color
+    
+end
 
-    def initialize(args)
-      @size = args[:size]
-      @tape_color = args[:tape_color]
-    end
+class RoadBike < Bicycle
+  attr_reader :size, :tape_color
 
-    def spares
-      {
-        chain: '10-speed',
-        tire_size: '23',
-        tape_color: tape_color
-      }
-    end
+  def initialize(args)
+    @size = args[:size]
+    @tape_color = args[:tape_color]
+  end
+
+  def spares
+    {
+      chain: '10-speed',
+      tire_size: '23',
+      tape_color: tape_color
+    }
+  end
 end
 
 class MountainBike < Bicycle
